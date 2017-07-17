a9648665c43d8c26f644ca98438f873dbc3196af
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2017-07-17 15:54:12 +0800

    generated splider without change

---
+++ scrapyProject/example/example/spiders/country.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from example.items import ExampleItem
+
+
+class CountrySpider(CrawlSpider):
+    name = 'country'
+    allowed_domains = ['example.webscraping.com']
+    start_urls = ['http://www.example.webscraping.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        i = ExampleItem()
+        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        #i['name'] = response.xpath('//div[@id="name"]').extract()
+        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        return i
